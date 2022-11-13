d45d362b5af26f6c523dc64a39889c869f63d8ff
kuba <kulajakub196@gmail.com>
2022-11-13 17:06:53 +0100

    gotowe logowanie razem z zabezpieczeniami

--- Nauka HTML/Logowanie do strony/game.php
+++ Nauka HTML/Logowanie do strony/game.php
@@ -1,17 +1,25 @@
+<?php
+    session_start();
 
+    if (!isset($_SESSION['loged'])) {
+        header('Location:index.php');
+        exit();
+    }
+?>
 
 <!doctype html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>...</title>
-	</head>
-	<body>
-<?php
-session_start();
 
-echo "<p> Witaj ". $_SESSION['login']. "!";
+<head>
+    <meta charset="UTF-8">
+    <title>...</title>
+</head>
 
-?>
-	</body>
+<body>
+    <?php
+
+    echo "<p> Witaj " . $_SESSION['login'] . '![<a href="logout.php">Wyloguj sie!</a>]</p>';
+    ?>
+</body>
+
 </html>
--- Nauka HTML/Logowanie do strony/index.php
+++ Nauka HTML/Logowanie do strony/index.php
@@ -1,14 +1,33 @@
+<?php
+session_start();
+if (isset($_SESSION['loged']) && ($_SESSION['loged'] == true)) {
+	header('Location:game.php');
+	exit();
+}
+?>
+
+
+
 <!doctype html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>...</title>
-	</head>
-	<body>
-	<form action = "login.php" method="post">
-		Login:<br/> <input type="text" name="login"/> <br/>
-		Password:<br/> <input type="password" name="password"/> <br/>
-		<input type="submit" calue="log in"/>
+
+<head>
+	<meta charset="UTF-8">
+	<title>...</title>
+</head>
+
+<body>
+	<form action="login.php" method="post">
+		Login:<br /> <input type="text" name="login" /> <br />
+		Password:<br /> <input type="password" name="password" /> <br />
+		<input type="submit" calue="log in" />
 	</form>
-	</body>
+
+	<?php
+	if (isset($_SESSION['blad'])) {
+		echo $_SESSION['blad'];
+	}
+	?>
+</body>
+
 </html>
